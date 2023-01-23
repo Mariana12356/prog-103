cd9dd8ce29313c39b538b902a75f483ebb8ead09
Mariana12356 <105241198+Mariana12356@users.noreply.github.com>
2023-01-23 14:06:23 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+Webcam.set({
+    width:350,
+    height: 300,
+    image_format : 'png',
+    png_quality:90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+function takeSnapshot(){
+    webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="captured_image" src = "'+data_uri+'"/>';
+    });
+    
+}
+
+console.log('versão ml5:', ml5.version);
+classifier= ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/Y2jIhqkOL/model.json',modelLoaded);
